b45c0fa66661e81879e1e3da58734b6efaecb26a
*vivek <vrvivekofficial@gmail.com>
2024-12-11 14:35:11 +0530

    connect to the database

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,20 @@
-import express from 'express'
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 
-const app = express()
+const app = express();
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Mongodb is connected");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
-})
+});
